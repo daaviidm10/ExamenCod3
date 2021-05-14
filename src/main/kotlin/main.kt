e22d3d47ba5f792e4035feeb7bb23fcef0720d60
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-05-14 10:20:48 +0200

    Update main.kt

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+<<<<<<< Ejer3
+import kotlin.random.Random
+=======
+>>>>>>> master
+import java.util.*
+import java.util.Arrays.toString as toString1
+
+fun main(args: Array<String>) {
+    for (tablamultiplicar in Ejer1(10).array) {
+        println(tablamultiplicar)
+<<<<<<< Ejer3
+
+    val array = Ejer2(10){Random.nextInt(50, 250)}
+        array.array.forEach { println(it) }
+    }
+    val array2 = Ejer3().listaImpares(Ejer2(10, {Random.nextInt(50, 250)}).array)
+=======
+    }
+>>>>>>> master
+}
