460199c35b186a25d911ebc23c732cb7307db222
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-05-14 10:10:28 +0200

    Ejercicio 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,13 @@
+import kotlin.random.Random
 import java.util.*
 import java.util.Arrays.toString as toString1
 
 fun main(args: Array<String>) {
     for (tablamultiplicar in Ejer1(10).array) {
         println(tablamultiplicar)
+
+    val array = Ejer2(10){Random.nextInt(50, 250)}
+        array.array.forEach { println(it) }
     }
+
 }
