d8db9b6a295023f4c8927b4e907a666c5307eeb1
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-05-14 10:25:40 +0200

    Ejercicio 2 porque me borró todo lo que tenía en la rama 2

---
+++ src/main/kotlin/Ejer2.kt
@@ -0,0 +1,3 @@
+   class Ejer2 (tamaño: Int, rellenaraleatorio : (Int) -> Int){
+        val array = Array(tamaño, rellenaraleatorio)
+    }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import java.util.*
 import java.util.Arrays.toString as toString1
 
@@ -5,4 +6,6 @@ fun main(args: Array<String>) {
     for (tablamultiplicar in Ejer1(10).array) {
         println(tablamultiplicar)
     }
+    val array = Ejer2(10){ Random.nextInt(50, 250)}
+    array.array.forEach { println(it) }
 }
